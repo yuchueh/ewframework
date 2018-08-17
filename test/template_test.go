50ff96bf7d6eb3b6a6d09ab33b267b3a694b4608
hehaifeng1984@126.com <hehaifeng1984@126.com>
2018-08-18 01:28:29 +0800

    +template action

--- test/template_test.go
+++ test/template_test.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os"
 	"testing"
+	"time"
 )
 
 func processTemplate01(w http.ResponseWriter, r *http.Request) {
@@ -48,6 +49,46 @@ func processTemplate05(w http.ResponseWriter, r *http.Request) {
 	temp.Execute(w, "Hello")
 }
 
+func formatDate(t time.Time) string {
+	layout := "2006-01-02"
+	return t.Format(layout)
+}
+
+func processTemplate06(w http.ResponseWriter, r *http.Request) {
+	sHtmlFile := osext.GetWd() + "html" + string(os.PathSeparator) + "tmpl06.html"
+	//fmt.Println(sHtmlFile)
+	funcsMap := template.FuncMap { "fdate": formatDate }
+	t := template.New("tmpl06.html").Funcs(funcsMap)
+	//temp, _ := template.ParseFiles(sHtmlFile, sHtmlFile)
+	//temp.Funcs(funcsMap)
+	//temp.Execute(w, time.Now())
+	t, _ = t.ParseFiles(sHtmlFile)
+	t.Execute(w, time.Now())
+}
+
+func processTemplate07(w http.ResponseWriter, r *http.Request) {
+	sHtmlFile := osext.GetWd() + "html" + string(os.PathSeparator) + "tmpl07.html"
+	temp, _ := template.ParseFiles(sHtmlFile)
+	context := `I asked: <i>"What's up?'"</i>`
+	temp.Execute(w, context)
+}
+
+func form(w http.ResponseWriter, r *http.Request) {
+	sHtmlFile := osext.GetWd() + "html" + string(os.PathSeparator) + "form.html"
+	temp, _ := template.ParseFiles(sHtmlFile)
+	temp.Execute(w, nil)
+}
+
+func processform(w http.ResponseWriter, r *http.Request) {
+	//stop the browser from protecting you from XSS attacks, you
+	//simply need to set a response header in our handler
+	w.Header().Set("X-XSS-Protection", "0")
+	sHtmlFile := osext.GetWd() + "html" + string(os.PathSeparator) + "processform.html"
+	temp, _ := template.ParseFiles(sHtmlFile)
+	temp.Execute(w, template.HTML(r.FormValue("comment")))
+	//temp.Execute(w, r.FormValue("comment"))
+}
+
 func Test_Template(t *testing.T) {
 	svr := http.Server{
 		Addr: "",
@@ -58,5 +99,9 @@ func Test_Template(t *testing.T) {
 	http.HandleFunc("/process03", processTemplate03)
 	http.HandleFunc("/process04", processTemplate04)
 	http.HandleFunc("/process05", processTemplate05)
+	http.HandleFunc("/process06", processTemplate06)
+	http.HandleFunc("/process07", processTemplate07)
+	http.HandleFunc("/form", form)
+	http.HandleFunc("/process", processform)
 	svr.ListenAndServe()
 }
